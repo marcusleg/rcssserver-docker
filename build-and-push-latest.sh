bcc11738c0379d10e9c552dfe138965081d2742a
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-29 12:36:59 +0200

    Remove `build-and-push-latest.sh` script

--- build-and-push-latest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-LATEST_VERSION=$(curl --silent https://api.github.com/repos/rcsoccersim/rcssserver/releases/latest | grep -Po '"tag_name": "\K.*?(?=")' | cut -d '-' -f 2)
-podman build --build-arg VERSION=$LATEST_VERSION -t orangefoil/rcssserver:$LATEST_VERSION -t orangefoil/rcssserver:latest .
-podman push orangefoil/rcssserver:$LATEST_VERSION docker://docker.io/orangefoil/rcssserver:$LATEST_VERSION
-podman push orangefoil/rcssserver:latest docker://docker.io/orangefoil/rcssserver:latest
